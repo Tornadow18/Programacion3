b61d1c30cf40fa5c21aa2fe3df24f6c5ac7067c2
Wilson Quintero <Wquintero@estudiante.uniajc.edu.co>
2025-02-11 16:33:39 +0000

    Operaciones con arreglos

---
+++ Clase2/OperacionesConArreglos.java
@@ -0,0 +1,11 @@
+public class OperacionesConArreglos {
+    public String mostrarElementos(int[] a){
+        String cad = "";
+        for (int i = 0; i < a.length; i++){ 
+        
+            System.out.println("a[ " + i + " ] = " + a[i] + "\n");
+        }
+            return cad;
+    }
+    
+}
