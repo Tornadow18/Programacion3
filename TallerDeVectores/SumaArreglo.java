0942398c3f3c362c4c835609180157b3f05dea1e
Wilson Quintero <Wquintero@estudiante.uniajc.edu.co>
2025-02-16 16:38:45 +0000

    Se agrego la suma del arreglo

--- TallerDeVectores/SumaArreglo.java
+++ TallerDeVectores/SumaArreglo.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
-public class SumaArreglo {
-    public static void main(String[] args) {
+public class SumaArreglo
+{
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese la cantidad de numeros (del vector):");
         int n = scanner.nextInt();
         int[] numeros = new int[n];
         System.out.println("Ingrese los "+ n +" numeros");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             numeros[i] = scanner.nextInt();
         }
         int suma = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             suma +=numeros[i];
         }
         System.out.println("La suma de los "+ n +"numeros es: "+suma);
