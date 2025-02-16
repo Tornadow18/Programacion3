a8cbe28839e97edbbe78d8d90c9b5d839e6adb54
Wilson Quintero <Wquintero@estudiante.uniajc.edu.co>
2025-02-16 17:12:20 +0000

    Se resolvió la pregunta 3 del taller

--- TallerDeVectores/SumaParesImpares.java
+++ TallerDeVectores/SumaParesImpares.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
 
-public class SumaParesImpares {
+public class SumaParesImpares 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
 
         // Leer la cantidad de números
-        System.out.println("Ingrese la cantidad de números: ");
+        System.out.println("Ingrese la cantidad de numeros: ");
         int n = scanner.nextInt();
 
         // Crear el array
         int[] numeros = new int[n];
 
         // Leer los números
-        System.out.println("Ingrese los números: ");
-        for (int i = 0; i < n; i++) {
+        System.out.println("Ingrese los numeros: ");
+        for (int i = 0; i < n; i++) 
+        {
             numeros[i] = scanner.nextInt();
         }
 
         // Calcular la suma de pares e impares
         int sumaPares = 0;
         int sumaImpares = 0;
-        for (int numero : numeros) {
-            if (numero % 2 == 0) {
+        for (int numero : numeros) 
+        {
+            if (numero % 2 == 0) 
+            {
                 sumaPares += numero;
-            } else {
+            } else 
+            {
                 sumaImpares += numero;
             }
         }
