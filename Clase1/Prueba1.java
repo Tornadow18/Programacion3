9736f620b8cac9615b0c52e138c685cedb0f8d5b
Wilson Quintero <Wquintero@estudiante.uniajc.edu.co>
2025-02-18 02:55:40 +0000

    Se resolvió la pregunta 9 del taller

--- Clase1/Prueba1.java
+++ Clase1/Prueba1.java
@@ -1,28 +1,44 @@
-Public class Prueba1 {
+public class Prueba1 {
 
-    public static String void main(String[] args)
-    {
-        int[] a = [2,5,8,3,1,10,11];
-        system.out.println(sumaParesImparesArreglo(a));
+    public static void main(String[] args) {
+        int[] a = { 4, 3, 4 };
+        System.out.println(sumaParesImparesArreglo(a));
+        System.out.println(sumaExtremosArreglo(a));
     }
 
-    Public static String sumaParesImparesArreglo(int[] a)
-    {
-        int sumapares = 0, sumaImpares = 0;
-        String cad="";
-        for(int i =  0; i < a.length; i++)
-        {
-            if(a[i]%2 == 0)
-            {
-                sumapares += a[i];
-                else
-                {
-                    sumaImpares += a[i];
-                }
+    public static String sumaParesImparesArreglo(int[] a) {
+        int sumaPares = 0, sumaImpares = 0;
+        String cad = "";
 
-            cad = "La suma de los números pares ="+sumapares+"Suma de los números impares = "+sumaImpares;
-            return cad;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                sumaPares += a[i];
+            } else {
+                sumaImpares += a[i];
             }
         }
+
+        cad = "La suma de los números pares = " + sumaPares + " suma de los números impares = " + sumaImpares;
+        return cad;
+    }
+
+    public static String sumaExtremosArreglo(int[] a) {
+        String cad = "";
+        
+        // Verificamos si el arreglo es par o impar
+        if (a.length % 2 == 0) {
+            // el arreglo es par
+            for (int i = 0; i < a.length / 2; i++) {
+                cad += (a[i] + (a[a.length - 1 - i])) + " ";
+            }
+        } else {
+            // el arreglo es impar
+            for (int i = 0; i < a.length / 2; i++) {
+                cad += (a[i] + (a[a.length - 1 - i])) + " ";
+            }
+            cad += a[a.length / 2];
+        }
+
+        return cad;
     }
 }
