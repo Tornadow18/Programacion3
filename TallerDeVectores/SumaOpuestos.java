37239f9223160302fcfd81557ce9d3a8fc57ccbc
Wilson Quintero <Wquintero@estudiante.uniajc.edu.co>
2025-02-18 03:03:11 +0000

    Se resolvió la pregunta 10 del taller

---
+++ TallerDeVectores/SumaOpuestos.java
@@ -0,0 +1,48 @@
+public class SumaOpuestos {
+
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de elementos del arreglo:");
+        int n = scanner.nextInt();
+        int[] arregloA = new int[n];
+        System.out.println("Ingrese los elementos del arreglo:");
+        for (int i = 0; i < n; i++) 
+        {
+            arregloA[i] = scanner.nextInt();
+        }
+
+        if (arregloA.length % 2 != 0) 
+        {
+            System.out.println("El arreglo debe tener un número par de elementos para calcular la suma de opuestos.");
+            return;
+        }
+
+        int[] arregloB = new int[arregloA.length / 2];
+
+        for (int i = 0; i < arregloA.length / 2; i++) 
+        {
+            arregloB[i] = arregloA[i] + arregloA[arregloA.length - 1 - i];
+        }
+
+        System.out.println("Arreglo resultante (B):");
+        imprimirArreglo(arregloB);
+
+        scanner.close();
+    }
+
+    static void imprimirArreglo(int[] arreglo) 
+    {
+        if (arreglo.length == 0) 
+        {
+            System.out.println("El arreglo está vacío.");
+        } else {
+            for (int elemento : arreglo) 
+            {
+                System.out.print(elemento + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
