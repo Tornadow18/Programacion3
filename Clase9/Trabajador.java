c89e24896f49099ca9e57076e61d56540cb840e3
Wilson Quintero <Wquintero@estudiante.uniajc.edu.co>
2025-04-01 16:12:31 +0000

    Se creo la clase trabajador

---
+++ Clase9/Trabajador.java
@@ -0,0 +1,15 @@
+package Clase9;
+
+public class Trabajador {
+    private String cedula;
+    private String nombre;
+    private double salario;
+    public Trabajador(String cedula, String nombre, double salario ){
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.salario = salario;
+
+
+
+    }
+}
