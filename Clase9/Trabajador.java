997759e694faf358d0dc7969caee16d5f9d0fec3
Wilson Quintero <Wquintero@estudiante.uniajc.edu.co>
2025-04-01 16:26:46 +0000

    Creacion de los metodos get, toString, Pagar

--- Clase9/Trabajador.java
+++ Clase9/Trabajador.java
@@ -9,7 +9,21 @@ public class Trabajador {
         this.nombre = nombre;
         this.salario = salario;
 
-
-
     }
-}
+    public String getCedula(){
+        return cedula;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public double getsalario(){
+        return salario;
+    }
+    public String toString(){
+        return "Trabajador {cedula:" +cedula+"Nombre: "+nombre+"Salario"+salario+"}"; 
+        }
+        public double pagar(double incremento){
+            return salario + (salario* incremento);
+        }
+    }
+
